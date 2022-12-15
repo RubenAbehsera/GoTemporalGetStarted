cb54393b3bd9ace063962092534fc73ef6df842c
ruben.abehsera <ruben.abehsera@eemi.com>
2022-12-15 12:08:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GoTemporalGetStarted
+
+Test of temporal.io tools to create a workflow. 
+
+Source : https://learn.temporal.io/getting_started/go/hello_world_in_go/
---
+++ start/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.temporal.io/sdk/client"
+	"hello-world-temporal/app"
+	"log"
+)
+
+func main() {
+	// Create the client object just once per process
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		log.Fatalln("unable to create Temporal client", err)
+	}
+	defer c.Close()
+
+	options := client.StartWorkflowOptions{
+		ID:        "greeting-workflow",
+		TaskQueue: app.GreetingTaskQueue,
+	}
+
+	// Start the workflow
+	name := "John"
+	we, err := c.ExecuteWorkflow(context.Background(), options, app.GreetingWorkflow, name)
+	if err != nil {
+		log.Fatalln("unable to complete Workflow result", err)
+	}
+
+	var greeting string
+	err = we.Get(context.Background(), &greeting)
+	if err != nil {
+		log.Fatalln("unable to get Workflow result", err)
+	}
+
+	printResults(greeting, we.GetID(), we.GetRunID())
+}
+
+func printResults(greeting string, workflowID, runID string) {
+	fmt.Printf("\nWorkflowID: %s RunID: %s\n", workflowID, runID)
+	fmt.Printf("\n%s\n\n", greeting)
+}
